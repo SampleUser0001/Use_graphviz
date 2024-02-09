6526ecee6ce9e1912c954e12822aa818e26654be
SampleUser0001 <ittimfn+github@gmail.com>
2024-02-09 22:36:57 +0900

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Use Graphviz
+
+## 概要
+
+Pythonでグラフを生成する。
+
+## 準備
+
+``` bash
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+```
+
+## 実行
+
+``` bash
+bash start.sh
+```
---
+++ app.py
@@ -0,0 +1,26 @@
+import graphviz
+from enum import Enum
+
+class Code(Enum):
+    A = 1
+    B = 2
+    C = 3
+    D = 4
+# Graphvizのオブジェクトを作成
+dot = graphviz.Digraph(format='png')
+
+# ノードを3つ作成
+dot.node(Code.A.name, 'A')
+dot.node(Code.B.name, 'B')
+dot.node(Code.C.name, 'C')
+dot.node(Code.D.name, 'D')
+
+# ノードを接続する
+dot.edge(Code.A.name, Code.B.name)
+dot.edge(Code.A.name, Code.D.name)
+dot.edge(Code.B.name, Code.C.name)
+dot.edge(Code.D.name, Code.C.name)
+
+# 表示
+dot.view()  # `dot`オブジェクトで`view`メソッドを呼び出す
+
